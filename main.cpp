35708350577afeb16d7410aa92d7d5b534ae0c2c
Reza Qorbani <reza3qorbani@gmail.com>
2021-01-16 16:47:32 +0100

    third commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout <<"Hello world!\n";
+    }
     std::cout <<"Hello world!\n"; 
     return 0;
+    
 }
