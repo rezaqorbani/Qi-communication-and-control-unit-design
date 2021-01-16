1046aafae7179081a1ee2752d4a0076c032272ad
Reza Qorbani <reza3qorbani@gmail.com>
2021-01-16 16:55:24 +0100

    repeated something

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ int main(){
     for (int i = 0; i < 10; i++)
     {
         std::cout <<"Hello world!\n";
+        std::cout <<"Hello world!\n";
     }
     std::cout <<"Hello world!\n"; 
     return 0;
