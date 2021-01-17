9e94e7643c50739843a75ec0db729484f28a6dca
Reza Qorbani <reza3qorbani@gmail.com>
2021-01-17 10:55:07 +0100

    create signal_processing header and cpp file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 
 int main(){
-    for (int i = 0; i < 10; i++)
-    {
-        std::cout <<"Hello world!\n";
-        std::cout <<"Hello world!\n";
-    }
-    std::cout <<"Hello world!\n"; 
+    
     return 0;
     
 }
