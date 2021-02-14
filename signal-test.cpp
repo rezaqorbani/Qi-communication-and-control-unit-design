14e2651439134d77ee623c1f7c0ded24c81c4f5d
Reza Qorbani <reza3qorbani@gmail.com>
2021-02-14 14:11:50 +0100

    added file for unit testing files

--- signal-test.cpp
+++ signal-test.cpp
@@ -2,7 +2,8 @@
 #include ".\ByteGenerator.h"
 #include ".\SignalGenerator.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n"; 
+    std::cout << "Hello World!" << argv[1] << '\n'; 
+    return 0;
 }
