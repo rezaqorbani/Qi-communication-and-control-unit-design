7ee3cc91891be80a7b75d7c9e17798d0d8afd9e3
Tova Stroeven <tova.stroeven@outlook.com>
2021-03-10 22:32:53 +0100

    added voltage cap and cleaned up the code

--- Qi-communication-control-unit.ino
+++ Qi-communication-control-unit.ino
@@ -12,7 +12,6 @@ namespace QiDelays
 	const int t_silent = 7; 
 	const int t_window = 8; //unit of 4ms, total 8 ms
 	const int t_offset = 4; //unit of 4ms, total 4ms
-	const int powerValueDelay = 1; // delay for taking a power sample
 
 }
 
@@ -26,8 +25,6 @@ namespace Pins
 	const int actualLoadConnect = 6; //When HIGH actual load and not dummy load is connected
 	const int loadLed1 = 7; 
 	const int loadLed2 = 8;
-    const int shuntPin1 = A2;
-    const int shuntPin2 = A5;
 };
 
 namespace Signals
@@ -43,6 +40,8 @@ namespace Signals
 
 
 float pingVoltage;
+char binaryCharArray[8] = {'0', '0','0', '0','0', '0','0', '0'}; 
+
 
 void sendSignal(SignalGenerator signal) 
 {
@@ -74,37 +73,25 @@ void sendSignal(SignalGenerator signal)
 }
 
 
-bool checkPing() // vet inte riktigt hur den ska implementeras eller vart den ska komma ifrån. kanske en global variabel?
+bool checkPing() 
 { 
-	float lowerLimitVoltage = 1.4;//defines the lower limit of our recitified current of the power signal and which level is considered on/off
-	double voltage = checkRectifiedAverage();//analogRead(Pins::rectifiedVoltagePin); 
-	pingVoltage = voltage; 
-	//Serial.println(voltage);
-	if(voltage<lowerLimitVoltage){
-		
-	//	Serial.println("failed");
-	}
+	float lowerLimitVoltage = 1.4;//defines the lower limit of our recitified current of the power signal and which level is considered high enough ping	
+	double voltage = checkRectifiedAverage();
+	//pingVoltage = voltage; 
 	return (voltage >= lowerLimitVoltage);
-   //return false;
 }
 bool checkRectified(){
 	float lowerLimitVoltage = 0.5;//defines the lower limit of our recitified current of the power signal and which level is considered on/off
-	double voltage = checkRectifiedAverage();//analogRead(Pins::rectifiedVoltagePin); 
-	//Serial.println(voltage);
-	//Serial.println();
+	double voltage = checkRectifiedAverage();
 	return (voltage >= lowerLimitVoltage);
 }
 
 bool checkOnSwitch()
-{
-	
-	bool isOn = digitalRead(Pins::onOffSwitchPin);
-	return isOn; //ÄNDRA sätt dedär kommentaren ovan
-	
+{ 
+	return (digitalRead(Pins::onOffSwitchPin));
 }
 
 //conver int to binary
-char binaryCharArray[8] = {'0', '0','0', '0','0', '0','0', '0'}; 
 void intToBinary(int n)
 {
 	int index = 7; 
@@ -112,72 +99,17 @@ void intToBinary(int n)
 	
 }
 
-void intToTwosComplement(int number)
-{
-	int copy = number; 
-	int index = 7; 
-	while(number!=0) {binaryCharArray[index] = (number%2==0 ?'0':'1'); index--; number/=2;}
-	if(copy < 0)
-	{
-		int i; 
-		int n = 8; 
-		for (i = n-1 ; i >= 0 ; i--) 
-			if (binaryCharArray[i] == '1') 
-				break; 
-	
-		// If there exists no '1' concatenate 1 at the 
-		// starting of string 
-		if (i == -1) 
-		{
-		binaryCharArray[7] = '1'; 
-			return; 
-		}
-		
-	
-		// Continue traversal after the position of 
-		// first '1' 
-		for (int k = i-1 ; k >= 0; k--) 
-		{ 
-			//Just flip the values 
-			if (binaryCharArray[k] == '1') 
-				binaryCharArray[k] = '0'; 
-			else
-				binaryCharArray[k] = '1'; 
-		} 
-	}	
-	
-    // return the modified string 
-    return; 
-} 
-
 
 bool oneOrHalfWatt()
 {
 	return (digitalRead(Pins::powerLevelSwitch)); 
 }
 
-double calculateVoltage()
-{
-	double totalInput1 = 0;//analogRead(Pins::shuntPin1);
-	double totalInput2 = 0;//analogRead(Pins::shuntPin2);
-	int repetitions = 10;
-	for(int i = 0; i < repetitions; i++){
-		totalInput1 = totalInput1 + analogRead(Pins::shuntPin1);
-		totalInput2 = totalInput2 + analogRead(Pins::shuntPin2); 
-	}
-	//double inputValue = analogRead(Pins::shuntPin1) - analogRead(Pins::shuntPin2);
-	double inputValue = totalInput1/repetitions - totalInput2/repetitions;
-	double voltage = inputValue * 5.0 / 1024.0;
-
-	return voltage; 
-}
-
-double calculatePower()
+double calculatePower()//Approximate value of the power based upon known resistance - this is ONLY used for Recivied Power Packet
 {
 	const double valueShunt = 1.2;
 	double loadVoltage = 3*checkRectifiedAverage();
-	double power = loadVoltage*loadVoltage/84;
-
+	double power = loadVoltage*loadVoltage/84;//84 ohms is an approximation of the resistance of the load
 	return power;
 }
 
@@ -187,22 +119,11 @@ double checkRectifiedAverage()
 	for(int i = 0; i < 10; i++){
 		totalVoltage = totalVoltage + analogRead(Pins::rectifiedVoltagePin); 
 	}
-	
 	double voltage = totalVoltage/10.0 * (5.0 / 1024.0);
-	Serial.println(3*voltage);
 	return voltage;
 }
 void setReceivedPowerMessage()
 {
-	//if want to recieve one watt
-	//Change the value of the message
-	//1/5 amp för 1 for one watt
-	//1/10 amp för 0.5 watt 
-	//t-d = t-a(1+c/128)
-	//t-d = t-a + t-a(c/128)
-	//t-d - t-a = t-a(c/128)
-	//128(t-d/t-a - 1) = c
-
 	double powerValues [8]; 
 	for(int i = 0; i < 8; i++)
 	{
@@ -227,7 +148,7 @@ void pingPhase()
 {
 	delay(QiDelays::t_wake); 
 
-	if(digitalRead(Pins::onOffSwitchPin)==0) //ÄNDRA sätt ett !
+	if(digitalRead(Pins::onOffSwitchPin)==0) 
 	{	
 		Signals::endPowerTransfer.setMessageIndex(0, ByteGenerator('0','0','0','0','0','0','0','1'));
 		sendSignal(Signals::endPowerTransfer);
@@ -235,24 +156,18 @@ void pingPhase()
 	}
 
 	else{
-		
-		double maxValue = 2.0; 
-		
-
-		// int sensorValue = analogRead(Pins::rectifiedVoltagePin);
-		// double voltage = analogRead(Pins::rectifiedVoltagePin)*5/1024; 
-
-
+		/*
+		//Alternative code below, this is more dynamic but does not really fulfill a purpose with this particular 
+		// transmitter. With a more diverse system it could be used in place of what is currently used
+		double maxValue = 2.0; //adapt depending on max ping value
 		int signalStrengthValue = (pingVoltage/maxValue)*256;
-		
-
 		if(signalStrengthValue >256)
 		{
 			signalStrengthValue = 255;
 		}
+		intToBinary(signalStrengthValue);
+		*/
 		intToBinary(255);
-		//intToBinary(signalStrengthValue);
-		
 		Signals::signalStrengthPacket.setMessageIndex(0, ByteGenerator(binaryCharArray));
 		sendSignal(Signals::signalStrengthPacket); 
 		delay(7);
@@ -263,11 +178,10 @@ void idConfigPhase()
 {
 	delay(2);
 
-	//Not completed
 	Signals::identificationPacket.setMessageIndex(0, ByteGenerator('0','0','0','1','0','0','1','0'));//Major/Minor version
-	Signals::identificationPacket.setMessageIndex(1, ByteGenerator('0','0','0','0','0','0','0','0'));//Påhittad manufacturer code del1
+	Signals::identificationPacket.setMessageIndex(1, ByteGenerator('0','0','0','0','0','0','0','0'));//manufacturer code del1
 	Signals::identificationPacket.setMessageIndex(2, ByteGenerator('0','0','0','0','0','0','0','1'));
-	//dessa nedan kan behöva ändras, beror på om vi behöver randomiza grejer
+	//device identifier
 	Signals::identificationPacket.setMessageIndex(3, ByteGenerator('1','0','0','0','0','0','0','0'));
 	Signals::identificationPacket.setMessageIndex(4, ByteGenerator('0','0','0','0','0','0','0','0'));
 	Signals::identificationPacket.setMessageIndex(5, ByteGenerator('0','0','0','0','0','0','0','0'));
@@ -293,55 +207,41 @@ void powerTransfer()
 	
 	bool loadDisconnected = true; 
 	digitalWrite(Pins::outputConnect,HIGH);
-	//digitalWrite(Pins::outputConnect,LOW);
-	
-
-
 	delay(90);
-	//Serial.println("output connected");
-	
-	// This if else claus is for changing the power level (0.5W to 1W)
-	
+		
 	while(checkRectified()&&checkOnSwitch())
 	
 	{
-		//Serial.println("pow");
 		int index = 0; 
-		double powerLevel; 
-		//int controlErrorValue;
-		if(digitalRead(Pins::powerLevelSwitch)){
-			powerLevel=1;
-			digitalWrite(Pins::loadLed1,HIGH);//Ändra
+		if(oneOrHalfWatt()){//power level 1W
+			digitalWrite(Pins::loadLed1,HIGH);
 			digitalWrite(Pins::loadLed2,HIGH);
 		}
-		else{
-			powerLevel = 0.5;
-			digitalWrite(Pins::loadLed1,LOW);
+		else{//Power level 0.5W
+			digitalWrite(Pins::loadLed1,LOW); 
 			digitalWrite(Pins::loadLed2,HIGH);
 		}
-		if(3*checkRectifiedAverage()>5.5 && loadDisconnected)
+		if(3*checkRectifiedAverage()>5.5 && loadDisconnected)//Sufficiently high voltage to connect our load
 		{
 			digitalWrite(Pins::actualLoadConnect,HIGH);
 			loadDisconnected = false;
 		}
 		while(checkRectified() && index <= 28)
 		{	
-			//double currentPower = calculatePower();
-			//double powerDifference = powerLevel - currentPower;
-			if(){
+			if(checkRectifiedAverage()*3>25){//Too much voltage, request a decrease (should in practice never occurr)
+				Signals::controlErrorPacket.setMessageIndex(0, ByteGenerator('1','0','0','0','0','0','0','0'));
+				sendSignal(Signals::controlErrorPacket);  
+				index++;
+				delay(40); 
 
 			}
-			else{
-
+			else{//Usually we need to request continusly more power since our system uses enough power to function, we just need to provide enough
+				Signals::controlErrorPacket.setMessageIndex(0, ByteGenerator('0','1','1','1','1','1','1','1'));
+				sendSignal(Signals::controlErrorPacket);  
+				index++;
+				delay(40); 
 			}
-			Signals::controlErrorPacket.setMessageIndex(0, ByteGenerator('0','1','1','1','1','1','1','1'));
-			sendSignal(Signals::controlErrorPacket);  
-			index++;
-			delay(40); 
-		} 
-		
-		
-		//The values below are preliminary and have to be changed in order 
+		} 		
 		if(checkRectified()){
 		setReceivedPowerMessage(); 
 		delay(QiDelays::t_offset);
@@ -364,8 +264,6 @@ void setup()
 	pinMode(Pins::actualLoadConnect, OUTPUT);
 	pinMode(Pins::loadLed1, OUTPUT);
 	pinMode(Pins::loadLed2, OUTPUT);
-	pinMode(Pins::shuntPin1, INPUT);
-	pinMode(Pins::shuntPin2, INPUT); 
 	Serial.begin(9600); 
 	digitalWrite(Pins::outputConnect,LOW);
 	//digitalWrite(Pins::outputConnect,HIGH);
@@ -393,9 +291,7 @@ void loop()
 
 	if(checkPing() && checkOnSwitch())
 	{
-	//Begin ping phase 
-		
-			
+	//Begin ping phase 	
 		pingPhase();
 	//END ping phase  
 
