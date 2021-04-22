3a1a9c376ff4f9bed5a8cfc9c745257c099882ad
Reza Qorbani <32905154+rezaqorbani@users.noreply.github.com>
2021-04-22 14:00:21 +0200

    add comments for functions

--- SignalGenerator.h
+++ SignalGenerator.h
@@ -12,19 +12,19 @@ Made by Reza Qorbani
 class SignalGenerator
 {
   public:
-    SignalGenerator(int header); //Enter hex value of the header to create a signal, the size of the message is then automatically calculated
-    void setMessageIndex(int index, ByteGenerator message); //set the message BYTE at the specified index (of all messages), set the message with a ByteGenerator object as argument (eg. ByteGenerator('1','0','1','1','1','0','1','0') note 8 bits) (write)
-    char* getMessageByte(int index); //get the message byte at the specified index (of all messages) (read)
-    char* getSignal(); // return the entire signal, every bit of it, in one array. You can iterate through this array by using a for loop for example and getSignalSize() for the size of the array.
-    int getSignalSize(); //return the size of the array that represents the signal.
+    SignalGenerator(int header); //Enter hex value of the header of a certain packet to create the packet, the size of the message of the packet is then automatically calculated and stored
+    void setMessageIndex(int index, ByteGenerator message); //Set the message BYTE at the specified index (of all messages), set the message with a ByteGenerator object as argument (eg. ByteGenerator('1','0','1','1','1','0','1','0') note 8 bits)
+    char* getMessageByte(int index); //get the message byte at the specified index (of all messages)
+    char* getSignal(); // return the entire packet, every bit of it, in one array. You can iterate through this array by using a for loop for example and getSignalSize() for the size of the array.
+    int getSignalSize(); //return the size of the array that represents the packet.
     ~SignalGenerator();
 
   private:
     char m_preamble [15] = {'1','1','1','1','1','1','1','1','1','1','1','1','1','1','1'};
-    ByteGenerator m_header_byte;
+    ByteGenerator m_header_byte;    
     int m_header_hex;               //the the numerical value of header (not the byte representing the header)
-    int m_messages_size;
-    char m_messages [7][11];
+    int m_messages_size;            // number of bytes used for message of the packet
+    char m_messages [7][11];        // a two dimensional array that can contain up to 7*11 bits, the maximum required for the messages
     ByteGenerator m_checksum = ByteGenerator('1','1','1','1','1','1','1','1');
     int m_calculate_message_size(); //calculates the size of the message
     void m_generate_checksum();  //Creates the checksum for the signal
